71b3a41cba81a97c8c69f3969cc53d3a69ea00e5
Arkadiusz Machalica <a.machalica@selleo.com>
2023-02-02 16:38:52 +0100

    WIP transfor imports to prevent barrel files from slowing down the tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
 /// <reference types="vitest" />
 //
 import react from '@vitejs/plugin-react'
+import transformImports from 'unplugin-transform-imports'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import svgrPlugin from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [mkcert(), react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    mkcert(),
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    // TODO configure this
+    // transformImports.vite({
+    //   parseOptions: {
+    //     plugins: ['typescript', 'jsx'],
+    //   },
+    //   modules: [
+    //     { path: 'lodash' },
+    //     {
+    //       path: '@mui/icons-material',
+    //       transform: `\${moduleName}/\${importName}`,
+    //     },
+    //     {
+    //       path: 'phosphor-react',
+    //       transform: (importName, moduleName) =>
+    //         `${moduleName}/dist/icons/${importName}.esm.js`,
+    //     },
+    //     {
+    //       path: 'mdi-material-ui',
+    //     },
+    //   ],
+    // }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
